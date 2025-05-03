111b8afd3db5c7db3baed35491bf3505ab8cc989
syauqqii <dimas.syauqi007@gmail.com>
2025-05-03 14:11:20 +0700

    Feat: Add .env file

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
+using DotNetEnv;
+using Microsoft.Extensions.Configuration;
+
 using fintrip.src.Routes;
 
+using System.Net;
+
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
+var appPortString = Environment.GetEnvironmentVariable("APP_PORT") ?? "5005";
+
+if (!int.TryParse(appPortString, out var appPort)) {
+    appPort = 5005;
+}
 
 builder.WebHost.ConfigureKestrel(options => {
-    options.ListenAnyIP(5005);
+    options.Listen(IPAddress.Any, appPort);
 });
 
 var app = builder.Build();
